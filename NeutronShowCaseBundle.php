153e50c374ba0f3dcc9dadd73608be48d0d30159
Nikolay Georgiev <azazen09@gmail.com>
2012-10-31 15:37:25 +0200

    Initial commit

---
+++ NeutronShowCaseBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Neutron\Plugin\ShowCaseBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NeutronShowCaseBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+NeutronShowCaseBundle
+=====================
+
+WARNING: This bundle is still under development.
